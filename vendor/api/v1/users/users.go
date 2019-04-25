4fc3cacdc7541ba426a68410b85068ea102eb3e5
Punto Damar P <punto.damar2@gmail.com>
2019-04-25 14:17:00 +0700

    remove unused import & comments

--- vendor/api/v1/users/users.go
+++ vendor/api/v1/users/users.go
@@ -3,19 +3,12 @@ package users
 import (
 	"github.com/gin-gonic/gin"
 
-	//"log"
-	//"net/http"
 	"fmt"
 
 	. "configs/database"
 	. "models/v1/user"
 )
 
-//type User struct {
-//	ID        int   `json:"id"`
-//	Name 	string `json:"name"`
-//
-//}
 
 func List(c *gin.Context) {
 	db 		:= Db
@@ -27,14 +20,6 @@ func List(c *gin.Context) {
 	} else {
 		c.JSON(200, users)
 	}
-
-	//if err := db.Find(&users).Error; err != nil {
-	//	c.AbortWithStatus(http.StatusInternalServerError)
-	//	fmt.Println(err)
-	//} else {
-	//	c.JSON(http.StatusOK, users)
-	//	log.Println(users)
-	//}
 
 	db.Close()
 }
