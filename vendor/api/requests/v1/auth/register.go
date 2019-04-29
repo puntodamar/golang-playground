f75f67d5ee7de8d45eb7966329843de4c8ac6f1f
Punto Damar P <punto.damar2@gmail.com>
2019-04-29 17:36:42 +0700

    bugfix register validator

--- vendor/api/requests/v1/auth/register.go
+++ vendor/api/requests/v1/auth/register.go
@@ -5,9 +5,9 @@ import (
 )
 
 type RegisterForm struct {
-	Name 		string `json:"name" 	valid:"required"`
+	Name 		string `json:"name" valid:"required"`
 	Username  	string `json:"username" valid:"required"`
-	Email		string `json:"email" 	valid:"email"`
+	Email		string `json:"email" valid:"email"`
 	Password 	string `json:"password" valid:"required,length(6|20)"`
 }
 
